c73eb7d278346524e049682a6fdcb078c0217a58
Gerard Hynes <gkhynes@gmail.com>
2020-05-04 11:13:28 +0100

    Update manifest name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `NASA APOD`,
+        short_name: `NASA APOD`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
