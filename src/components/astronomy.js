3ec4c1c2c6495810c9ef15294cc5d3626cdb8957
Gerard Hynes <gkhynes@gmail.com>
2020-05-04 10:39:18 +0100

    Add Video component to handle videos

--- src/components/astronomy.js
+++ src/components/astronomy.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
+import Video from "../components/video"
 import DatePicker from "react-datepicker"
 import axios from "axios"
+
 import { format } from "date-fns"
 import "./astronomy.css"
 import "react-datepicker/dist/react-datepicker.css"
@@ -10,7 +12,8 @@ export default function Astronomy() {
   const [date, setDate] = useState("")
   const [explanation, setExplanation] = useState("")
   const [title, setTitle] = useState("")
-  const [imageUrl, setImageUrl] = useState("")
+  const [mediaUrl, setMediaUrl] = useState("")
+  const [mediaType, setMediaType] = useState("")
   const [startDate, setStartDate] = useState(new Date())
   const searchDate = format(startDate, "yyyy-MM-dd")
 
@@ -21,16 +24,33 @@ export default function Astronomy() {
       const result = await axios.get(
         `https://api.nasa.gov/planetary/apod?api_key=TrOS4UtSVKBOtq0fzZL5PBrjUdtB4oDbgt5DFDdc&date=${searchDate}`
       )
-      const { copyright, date, explanation, title, url } = result.data
+      const {
+        copyright,
+        date,
+        explanation,
+        title,
+        url,
+        media_type,
+      } = result.data
 
       setCopyright(copyright)
       setDate(date)
       setExplanation(explanation)
       setTitle(title)
-      setImageUrl(url)
+      setMediaUrl(url)
+      setMediaType(media_type)
     }
     fetchImage()
-  }, [copyright, date, explanation, title, imageUrl, startDate, searchDate])
+  }, [
+    copyright,
+    date,
+    explanation,
+    title,
+    mediaUrl,
+    mediaType,
+    startDate,
+    searchDate,
+  ])
 
   return (
     <div className="Astronomy">
@@ -46,7 +66,11 @@ export default function Astronomy() {
       <h2>
         The image for {printDate(date)} is {title}
       </h2>
-      <img src={imageUrl} alt={title} />
+      {mediaType === "video" ? (
+        <Video videoSrcURL={mediaUrl} videoTitle={title} />
+      ) : (
+        <img src={mediaUrl} alt={title} />
+      )}
       <h4>
         <span>{copyright}</span>
         {` `}
---
+++ src/components/video.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./video.css"
+
+const Video = ({ videoSrcURL, videoTitle }) => (
+  <div className="Video">
+    <iframe
+      src={videoSrcURL}
+      title={videoTitle}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      frameBorder="0"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true"
+      allowFullScreen
+    />
+  </div>
+)
+export default Video
